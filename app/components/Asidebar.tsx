e95b32133a5b9b635f40091c43285dd9c7808c06
spunktsch <me@spunktsch.com>
2023-11-23 23:01:59 +0100

    add back model count

--- app/components/Asidebar.tsx
+++ app/components/Asidebar.tsx
@@ -10,7 +10,7 @@ export default function Asidebar({ counts }: AsidebarProps) {
   return (
     <aside className="flex-1 p-3 xl:ml-10 xl:p-0 flex-grow">
       <div className="w-full xl:max-w-[300px] sticky top-10">
-        {/* <ModelListCountTitle counts={counts} className="lg:text-2xl border-b border-white/10 pb-10" /> */}
+        <ModelListCountTitle counts={counts} className="lg:text-2xl border-b border-white/10 pb-10" />
         <ArticlesBlock />
       </div>
     </aside>
